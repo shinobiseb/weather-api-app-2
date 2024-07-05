a6eb072816307438cb0e3158e5a4f580cc2600b1
shinobiseb <sebpatin@gmail.com>
2024-07-05 18:01:25 -0400

    added Refs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,31 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, useRef } from 'react'
 
-const handleChange = (setState: React.Dispatch<React.SetStateAction<number>>) => (event: ChangeEvent<HTMLInputElement>) => {
-  
-  const value = event.target.value
-  const number = parseInt(value)
 
-  setState(number);
-};
+
+
 
 export default function Input() {
 
+  const handleChange = (setState: React.Dispatch<React.SetStateAction<number>>) => (event: ChangeEvent<HTMLInputElement>) => {
+  
+    const value = event.target.value
+    const number = parseInt(value)
+
+    setState(number);
+  };
+
+  function getCoords() {
+
+    let lat = latRef.current
+    let lon = lonRef.current
+
+    console.log(lat, lon)
+  }
+
+  const latRef = useRef(null)
+  const lonRef = useRef(null)
   const [latInput, setLatInput] = useState(0)
   const [lonInput, setLonInput] = useState(0)
-
-  
 
   return (
     <div className='w-full flex flex-row justify-center'>
@@ -22,6 +34,7 @@ export default function Input() {
         value={latInput}
         className='px-2 border-black border w-20 rounded-md text-black' 
         type="number"
+        ref={latRef}
         placeholder='Latitude'
       />
       <input 
@@ -29,9 +42,10 @@ export default function Input() {
         value={lonInput}
         className='px-2 border-black border w-20 rounded-md text-black' 
         type="number"
+        ref={lonRef}
         placeholder='Latitude'
       />
-      <button className="confirm bg-white px-2 p-1 rounded-md text-black">Confirm</button>
+      <button onClick={getCoords} className="confirm bg-white px-2 p-1 rounded-md text-black">Confirm</button>
     </div>
   )
 }
