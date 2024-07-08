27c7703843fe3f8bcf489786cde5b819faf18acd
shinobiseb <sebpatin@gmail.com>
2024-07-08 19:16:19 -0400

    header row

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 
 export default function Header() {
   return (
-    <header>
-        <h1 className='header-title text-2xl'>
+    <header className="flex flex-row justify-between p-2 items-center mb-2">
+        <h1 className='header-title text-xl'>
             CWB
         </h1>
         <p>Cloudy With a Chance of Bars</p>
