3d68be690377e26d5b08c2738cdefa8d548e7e2f
shinobiseb <sebpatin@gmail.com>
2024-07-12 22:51:02 -0400

    https instead of http

--- src/assets/endpoints.tsx
+++ src/assets/endpoints.tsx
@@ -3,10 +3,10 @@ export const endpoints = {
       return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=${units}`;
     },
     getCoordsByZip: (zip: string | null, apiKey: string): string => { // Renamed function
-      return `http://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=${apiKey}`;
+      return `https://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=${apiKey}`;
     },
     getCoordsByCityName: (cityName: string | null, apiKey: string): string => {
-      return `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${apiKey}`;
+      return `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${apiKey}`;
     },
   };
   
