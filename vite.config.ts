f8497e2821754d7a57709918eb59a44eadde858f
shinobiseb <sebpatin@gmail.com>
2024-07-12 22:37:01 -0400

    config updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://shinobiseb.github.io/weather-api-app-2/",
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
